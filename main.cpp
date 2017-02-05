ec60753bf65ea4bdfc8c6b250e9976da5b020ea9
Neil Johnson <mr.neil.j@gmail.com>
2017-02-04 22:46:50 -0800

    added Movie.hpp / .cpp completed main and confirmed application could run

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include "Movie.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    Movie mov1("Sharktopus", "Charlie Chaplin", 1000, 503);
+    mov1.printInfo();
+
+    mov1.setDirector("Winston Churchill");
+    mov1.printInfo();
+
+    Movie mov2;
+    mov2.printInfo();
+
     return 0;
 }
